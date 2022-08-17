e0d7709c78f86d1b0dc2405133869066863a422e
ZeyiLiao <zeyiliao@usc.edu>
2022-08-17 12:27:06 -0700

    test

--- main.py
+++ main.py
@@ -204,7 +204,7 @@ def main(args):
 
     print(f'We are using {device}')
 
-
+# This is test
 
     retrieve = Retrieve(all_heads_path,save_embedding_path,all_tuples_path,device,save)
     with open(query_path,'r') as f:
