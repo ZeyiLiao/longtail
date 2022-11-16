4fbb2e92d4ec0179d34a7e758c0a7574601c90cd
ZeyiLiao <zeyiliao@usc.edu>
2022-11-15 16:41:13 -0800

    clear structure

--- sample_command.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python main.py --combine_order normal
-
-python main.py --combine_order reverse
--- test2.py
+++
@@ -1,3 +0,0 @@
-from datasets import load_dataset
-ds = load_dataset('json', data_files='/home/zeyi/longtail/longtail_data/for_finetune/property_centric/wo_m_t5/train/data.json')
-print(ds)
