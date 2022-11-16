d0241d2a7377fbfd1abb92ff3b8dd49b06d6b0fb
ZeyiLiao <zeyiliao@usc.edu>
2022-11-16 10:44:40 -0800

    1

--- test_folder/print_1.py
+++ test_folder/print_1.py
@@ -1,2 +1,6 @@
+import os
+print('This is print_1')
+print(os.getcwd())
+
 def print_func():
     print(1)
--- test_folder/test.py
+++ test_folder/test.py
@@ -1,5 +1,8 @@
-from test_folder import print_1
+import os
+print(os.getcwd())
+# from test_folder import print_1
 
-# import print_1
+
+import print_1
 
 print_1.print_func()
