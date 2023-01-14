d0674dbb0a34085f537479eb7c48f610151fba1d
ZeyiLiao <zeyiliao@usc.edu>
2023-01-13 20:06:17 -0800

    1

--- test_cli.py
+++ test_cli.py
@@ -4,6 +4,7 @@ name = "gpt2"
 t = AutoTokenizer.from_pretrained(name)
 m = AutoModelForCausalLM.from_pretrained(name)
 
+
 sent = "I want to go"
 enc = t(sent,return_tensors= "pt")
 generation = m.generate(**enc,typical_p = 0.9,do_sample = True)
